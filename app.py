defc7936489deb6ee013070136aa354edbcb47e0
akipg <108157287+akipg@users.noreply.github.com>
2024-10-06 09:23:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,55 @@
 import streamlit as st
 from streamlit_terminal import st_terminal
 
-# set wide mode
+# Set wide mode
 st.set_page_config(layout="wide")
 
+st.markdown("🚧 **Work in Progress** 🚧")
 
 st.markdown("# Streamlit Terminal")
 
+# Example 1: Basic Usage
 st.markdown("## Example 1: Basic Usage")
-
+st.write("Use this terminal to execute commands.")
+st.write("For example, type `ls` after the `>` prompt and press `Enter` to run the command.")
 st_terminal(key="terminal1", show_welcome_message=True)
 
+st.markdown("""
+            ```python
+            # Source code of the above example
+            from streamlit_terminal import st_terminal
+            st_terminal(key="terminal1", show_welcome_message=True)
+            ```""")
 
+# Example 2: Custom Terminal
 st.markdown("## Example 2: Custom Terminal")
 
-st.markdown("### Auto height")
-st.write("This terminal has a command `python -u test/clock.py` and height < 0 (auto height)")
-st_terminal(key="terminal2", command="python -u test/clock.py", height=-1)
-
-st.markdown("### Fixed height")
-st.write("This terminal has a command `echo 'Hello World'` and height 200px")
-st_terminal(key="terminal3", command="echo 'Hello World'", height=200)
-
-st.markdown("### Set command from input")
-st.write("This terminal has an external command input")
+st.markdown("### Set a Command from Input")
+st.write("In this terminal, you can't type commands directly, but you can set a custom command using the `command` parameter.")
+st.write("Click the |> button at the top-right of the terminal to run the command.")
 cmd = st.text_input("Command", "python -u test/clock.py")
-st_terminal(key="terminal4", command=cmd, height=-1, disable_input=True)
+st_terminal(key="terminal4", command=cmd, height=-1, max_height=250, disable_input=True)
 
-
+# Example 3: Get Output from Terminal
 st.markdown("## Example 3: Get Output from Terminal")
 
-full_outputs, updated_outputs = st_terminal(key="terminal5",  command="python -u test/clock.py", height=-1)
+full_outputs, updated_outputs = st_terminal(key="terminal5", command="python -u test/clock.py", height=200)
 
-st.markdown("### Updated outputs")
-st.write(updated_outputs)
+with st.container(height=500):
+    cols = st.columns(2)
+    with cols[0]:
+        st.markdown("### Full Outputs")
+        st.write(full_outputs)
 
-st.markdown("### Full outputs")
-st.write(full_outputs)
+    with cols[1]:
+        st.markdown("### Updated Outputs")
+        st.write(updated_outputs)
+
+
+# Example 4: Colorful Output
+st.markdown("## Example 4: Colorful Output")
+st.write("This terminal displays colorful output.")
+colorful_command = st.text_input("Colorful command", r'''echo -e "\x1b[31mError: Something went wrong\x1b[0m" && 
+echo -e "\x1b[33mWarning: Check your configuration\x1b[0m" && 
+echo -e "\x1b[32mSuccess: Operation completed successfully\x1b[0m"''')
+st_terminal(key="terminal2", command=colorful_command, height=-1)
